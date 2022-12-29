b1ee08773e13e94c47d57cbbc44de1feb6fac8c6
AdarDesu <adartruusa@gmail.com>
2022-12-29 23:49:59 +0200

    Week 16

---
+++ Week16/BitCoinRate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week16
+{
+    public class BitCoinRate
+    {
+        public bpi bpi { get; set; }
+        public time time { get; set; }
+    }
+
+    public class time
+    {
+        public string updated { get; set; }
+    }
+
+    public class bpi
+    {
+        public EUR EUR { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+}
---
+++ Week16/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using Week16;
+
+BitCoinRate BitCoin = GetRates();
+Console.WriteLine($"Current BitCoin rate is {BitCoin.bpi.EUR.rate_float} {BitCoin.bpi.EUR.code}. " +
+    $"It was updated in {BitCoin.time.updated}.");
+
+
+static BitCoinRate GetRates()
+{
+    string Url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+    HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(Url);
+    Request.Method = "GET";
+
+    var WebResponse = Request.GetResponse();
+    var WebStream = WebResponse.GetResponseStream();
+
+    BitCoinRate BitcoinRate;
+
+    using (var ResponseReader = new StreamReader(WebStream))
+    {
+        var Response = ResponseReader.ReadToEnd();
+
+        BitcoinRate = JsonConvert.DeserializeObject<BitCoinRate>(Response);
+    }
+
+    return BitcoinRate;
+
+}
